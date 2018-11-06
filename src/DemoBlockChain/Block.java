f80ca9e4b5f5fe3e523b3b953ef08165ae0676c1
vandungitars <levandung7549@gmail.com>
2018-11-06 12:58:15 +0700

    first commit

---
+++ src/DemoBlockChain/Block.java
@@ -0,0 +1,33 @@
+package DemoBlockChain;
+
+import java.util.Date;
+
+public class Block{
+	public String hash;
+	public String previousHash;
+	public String data;
+	private long timeStamp;
+	private int nonce;
+	
+	public Block(String data, String previousHash) {
+		this.data = data;
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+		this.hash = calculateHash();
+	}
+	
+	public String calculateHash() {
+		String calculatedhash = StringUtil.applySha256(
+				previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + data
+		);
+		return calculatedhash;
+	}
+	public void mineBlock(int difficulty) {
+		String target = StringUtil.getDifficultyString(difficulty);
+		while(hash.substring(0, difficulty).equals(target)) {
+			nonce++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
+	}
+}
